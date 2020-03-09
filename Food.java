3cd30c1f72e9a8dcad9ee962b61164225fbd283d
ErinnnnnL <61876876+ErinnnnnL@users.noreply.github.com>
2020-03-09 01:42:48 -0400

    Update Food.java

--- Food.java
+++ Food.java
@@ -5,6 +5,9 @@ public class Food extends Items{
     private ArrayList<String> icon = new ArrayList<String>();
     public void fillArray(){
         icon.add("burger.png");
+        icon.add("soda.png");
+        icon.add("carrot.png");
+        icon.add("strawberry.png");
     }
     public Food(int x, int y, String f, int v){
         super(x,y,f,v);
@@ -12,10 +15,12 @@ public class Food extends Items{
 
     public int PointVal() {
         for(int i = 0; i<icon.size(); i++){
-            if (super.getF().equals(icon.get(i))){
-                return -1;
-            } else {
-                return 1;
+            if (super.getF().equals(icon.get(i)) ){
+                if(i==0 || i ==1){
+                    return -1;
+                } else {
+                    return 1;
+                }
             }
         }
         return 1;
