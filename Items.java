58a3776d141a5a9ca97a3b123728a9582743d13c
skumaran2021 <44066066+skumaran2021@users.noreply.github.com>
2020-03-10 11:18:48 -0400

    Update Items.java

--- Items.java
+++ Items.java
@@ -1,17 +1,36 @@
 import javax.swing.*;
+import java.awt.*;
+
 public abstract class Items {
     private int centerX, centerY;
     private int velocity;
+    private String itemName;
+    private boolean visible;
     private static int panelWidth;
     private ImageIcon image; //import image from file
     public Items(int x, int y, String f, int v){
         centerX = x;
         centerY = y;
         velocity = v;
+        itemName = f;
+        visible=true;
         image = new ImageIcon(f);
     }
+    public ImageIcon getImageIcon(){
+      return image;
+    }
+    public void draw(Graphics g)
+    {
+         paintIcon(this, g, getX(), getY());
+    }
+    public void hide(){
+        visible = false;
+    } 
     public static void setPanelWidth(int w){
         panelWidth = w;
+    }
+    public String getItemName(){
+        return itemName;
     }
     public int getX(){
         return centerX;
@@ -22,30 +41,20 @@ public abstract class Items {
     public void setVelocity(int v){
         velocity = v;
     }
-    public int getVelocity()
-    {
+    public int getVelocity(){
         return velocity;
     }
+    public boolean containsPoint(int x, int y){ //If the mouse with the person touches food item then disappears
+        int xSquared = (x - centerX) * (x - centerX);
+        int ySquared = (y - centerY) * (y - centerY);
+        int area = image.getIconWidth() * image.getIconHeight();
+        return xSquared + ySquared - area <= 0;
+    }
     public void move(){ //need smth that moves down with a set velocity
-        /**int xVal = getX();
-
-        if(xVal + radius > panelWidth){  //include getWidth() so we bounce off on the right edge
-
-            direction=0; //negative;				
-            xVal-=velocity;
+        int yVal = getY();
+        if(yVal + image.getIconWidth() > panelWidth){  //include getWidth() so we bounce off on the right edge				
+            yVal+=velocity; //Going down
         }
-        else if(xVal - radius < 0){
-
-            xVal+=velocity;
-            direction = 1; //positive
-        }
-        else
-        {
-            if(direction == 1)
-                xVal+=velocity;
-            else
-                xVal-=velocity;
-        }
-        centerX = xVal;*/
+        centerY = yVal;
     }
 }
