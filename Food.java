66cd8b0d4f5173b03b39b2b0e062e0840446fd47
skumaran2021 <44066066+skumaran2021@users.noreply.github.com>
2020-03-10 11:18:20 -0400

    Update Food.java

--- Food.java
+++ Food.java
@@ -1,13 +1,12 @@
 import java.util.ArrayList;
 
-
 public class Food extends Items{
     private ArrayList<String> icon = new ArrayList<String>();
     public void fillArray(){
         icon.add("burger.png");
         icon.add("soda.png");
-        icon.add("carrot.png");
         icon.add("strawberry.png");
+        icon.add("carrot.png");
     }
     public Food(int x, int y, String f, int v){
         super(x,y,f,v);
@@ -15,14 +14,12 @@ public class Food extends Items{
 
     public int PointVal() {
         for(int i = 0; i<icon.size(); i++){
-            if (super.getF().equals(icon.get(i)) ){
-                if(i==0 || i ==1){
-                    return -1;
-                } else {
-                    return 1;
-                }
+            if (super.getItemName().equals(icon.get(i)) && i <= 1){
+                return -1;
+            } else {
+                return 1;
             }
         }
-        return 1;
+        return 0;
     }
 }
