f3a53dd86f6f4f45b18bc0b69b6374bfe579f472
skumaran2021 <44066066+skumaran2021@users.noreply.github.com>
2020-03-13 18:20:33 -0400

    Update Interests.java

--- Interests.java
+++ Interests.java
@@ -3,7 +3,8 @@ import java.util.ArrayList;
 
 public class Interests extends Items{
     private ArrayList<String> icon = new ArrayList<String>();
-    public void fillArray(){
+    private int centerX, centerY;
+    private void fillArray(){
         icon.add("phone.png");
         icon.add("gaming.png");
         icon.add("sports.png");
@@ -11,11 +12,35 @@ public class Interests extends Items{
     }
     public Interests(int x, int y, String f, int v){
         super(x,y,f,v);
+        fillArray();
+    }
+    
+    public void setCenters() {
+        if (getItemName().equals("phone.png")) {
+            centerX = getX() + 44;
+            centerY = getY() + 43;
+        } else if (getItemName().equals("gaming.png")) {
+            centerX = getX() + 45;
+            centerY = getY() + 39;
+        } else if (getItemName().equals("sports.png")) {
+            centerX = getX() + 52;
+            centerY = getY() + 49;
+        } else if (getItemName().equals("music.png")) {
+            centerX = getX() + 49;
+            centerY = getY() + 47;
+        }
+    }
+    
+    public int getItemCenterX() {
+        return centerX;
+    }
+    public int getItemCenterY() {
+        return centerY;
     }
 
     public int PointVal() {
-        for(int i = 0; i<icon.size(); i++){
-            if (super.getF().equals(icon.get(i)) ){
+        for(int i = 0; i < 4; i++){
+            if (getItemName().equals(icon.get(i)) ){
                 if(i==0 || i ==1){
                     return -1;
                 } else {
@@ -23,6 +48,6 @@ public class Interests extends Items{
                 }
             }
         }
-        return 1;
+        return 0;
     }
 }
