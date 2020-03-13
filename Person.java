b3798b8a2ad3904f9872a9356d62626048faa06d
skumaran2021 <44066066+skumaran2021@users.noreply.github.com>
2020-03-13 18:22:03 -0400

    Update Person.java

--- Person.java
+++ Person.java
@@ -2,14 +2,27 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Person {
-    private int centerX, centerY;
+    private int centerX, centerY; //image
+    private int personCenterX, personCenterY;
     private static int panelWidth;
     private boolean visible;
     private ImageIcon image; //import image from file
-    public Person(int x, int y, String f){
+    public Person(int x, int y, int stage, String f){
         centerX = x;
         centerY = y;
         image = new ImageIcon(f);
+        
+        if (stage == 1) {
+           personCenterX = x + 73; 
+           personCenterY = y + 61;
+        } else if (stage == 2) {
+           personCenterX = x + 62;
+           personCenterY = y + 75;
+        } else if (stage == 3) {
+           personCenterX = x + 60;
+           personCenterY = y + 58;
+        }
+           
     }
     public ImageIcon getImageIcon(){
       return image;
@@ -27,24 +40,37 @@ public class Person {
     public static void setPanelWidth(int w){
         panelWidth = w;
     }
-    public boolean containsPoint(int x, int y){
-        int xSquared = (x - centerX) * (x - centerX);
-        int ySquared = (y - centerY) * (y - centerY);
-        double area = image.getIconWidth() * image.getIconHeight();
-        return xSquared + ySquared - area<= 0; 
+    public boolean containsPoint(int stage, int x, int y){
+   
+         double rightSide = 0, leftSide = 0, top = 0, bottom = 0;
+        if (stage == 1){
+            rightSide = personCenterX + 50;
+            leftSide = personCenterX - 50;
+            top = personCenterY - 53;
+            bottom = personCenterY + 45;
+        } else if (stage == 2) {
+            rightSide = personCenterX + 58;
+            leftSide = personCenterX - 55;
+            top = personCenterY - 66;
+            bottom = personCenterY + 44;
+        } else if (stage == 3) {
+            rightSide = personCenterX + 36;
+            leftSide = personCenterX - 39;
+            top = personCenterY - 38;
+            bottom = personCenterY + 57;
+        }
+        
+        return x <= rightSide && x >= leftSide && y >= top && y <= bottom;
+         
     }
-    
-    /*public boolean containsFoodPoint(int x, int y){ //Fix this eventually
-        int xSquared = (x - centerX) * (x - centerX);
-        int ySquared = (y - centerY) * (y - centerY);
-        double area = image.getIconWidth() * 2 * Math.PI;
-        return xSquared + ySquared - area + 100 <= 0;
-    }*/
     public int getX(){
         return centerX;
     }
     public int getY(){
         return centerY;
+    }
+    public void setCenterX(int x) {
+        personCenterX = x;
     }
     public void moveLeft()
     {
@@ -54,10 +80,7 @@ public class Person {
     {
         centerX+=2;
     }
-    public void move(int x, int w){//moves side to side
-        centerX = x - w; //setX(x - w);
-    }
-    public void moveMouse(int dx, int dy)
+    public void moveMouse(int dx)
     {
         centerX += dx;
         //dy is not needed
