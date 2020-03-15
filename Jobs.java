441c8f8816f6fea405bc9c7ffb08a2501aacc20e
skumaran2021 <44066066+skumaran2021@users.noreply.github.com>
2020-03-15 11:10:35 -0400

    Update Jobs.java

--- Jobs.java
+++ Jobs.java
@@ -3,19 +3,44 @@ import java.util.ArrayList;
 
 public class Jobs extends Items{
     private ArrayList<String> icon = new ArrayList<String>();
+    private int centerX, centerY;
     public void fillArray(){
-        icon.add("murder.png");
+        icon.add("murderer.png");
         icon.add("robber.png");
         icon.add("fortuneCookie.png");
-        icon.add("foodTaster.png");
+        icon.add("traveler.png");
     }
     public Jobs(int x, int y, String f, int v){
         super(x,y,f,v);
+        fillArray();
+    }
+    
+    public void setCenters() {
+        if (getItemName().equals("murderer.png")) {
+            centerX = getX() + 49;
+            centerY = getY() + 51;
+        } else if (getItemName().equals("robber.png")) {
+            centerX = getX() + 51;
+            centerY = getY() + 61;
+        } else if (getItemName().equals("fortuneCookie.png")) {
+            centerX = getX() + 45;
+            centerY = getY() + 42;
+        } else if (getItemName().equals("traveler.png")) {
+            centerX = getX() + 46;
+            centerY = getY() + 52;
+        }
+    }
+    
+    public int getItemCenterX() {
+        return centerX;
+    }
+    public int getItemCenterY() {
+        return centerY;
     }
 
     public int PointVal() {
         for(int i = 0; i<icon.size(); i++){
-            if (super.getF().equals(icon.get(i)) ){
+            if (super.getItemName().equals(icon.get(i)) ){
                 if(i==0 || i ==1){
                     return -1;
                 } else {
@@ -23,6 +48,6 @@ public class Jobs extends Items{
                 }
             }
         }
-        return 1;
+        return 0;
     }
 }
