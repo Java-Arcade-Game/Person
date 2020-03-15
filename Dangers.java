ba66ded465f18f87982fd5a0c9a012b8bef519f6
skumaran2021 <44066066+skumaran2021@users.noreply.github.com>
2020-03-15 12:42:21 -0400

    Update Dangers.java

--- Dangers.java
+++ Dangers.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 
 public class Dangers extends Items{
     private ArrayList<String> icon = new ArrayList<String>();
+    private int centerX, centerY;
     public void fillArray(){
         icon.add("pineapple.png");
         icon.add("lightning.png");
@@ -11,11 +12,35 @@ public class Dangers extends Items{
     }
     public Dangers(int x, int y, String f, int v){
         super(x,y,f,v);
+        fillArray();
+    }
+    
+    public void setCenters() {
+        if (getItemName().equals("pineapple.png")) {
+            centerX = getX() + 50;
+            centerY = getY() + 51;
+        } else if (getItemName().equals("lightning.png")) {
+            centerX = getX() + 45;
+            centerY = getY() + 46;
+        } else if (getItemName().equals("laughing.png")) {
+            centerX = getX() + 48;
+            centerY = getY() + 45;
+        } else if (getItemName().equals("exercise.png")) {
+            centerX = getX() + 49;
+            centerY = getY() + 50;
+        }
+    }
+    
+    public int getItemCenterX() {
+        return centerX;
+    }
+    public int getItemCenterY() {
+        return centerY;
     }
 
     public int PointVal() {
         for(int i = 0; i<icon.size(); i++){
-            if (super.getF().equals(icon.get(i)) ){
+            if (super.getItemName().equals(icon.get(i)) ){
                 if(i==0 || i ==1 || i==2){
                     return -1;
                 } else {
@@ -23,6 +48,6 @@ public class Dangers extends Items{
                 }
             }
         }
-        return 1;
+        return 0;
     }
 }
